a8c6e3d8e5d0429cff22de60f40bff7aebf1b990
vknanavati <vimin.nanavati@gmail.com>
2024-07-30 15:05:05 -0400

    styled Heading, Buttons, and Count with MUI.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import {Container, Grid, Typography, Button} from '@mui/material';
+
 function App() {
 
   const [count, setCount] = useState(0);
@@ -12,14 +14,78 @@ function App() {
   }
 
   return (
-    <>
-      <h1>Counter App</h1>
-      <button
-        onClick={handleSubtract}>-</button>
-      <span>{count}</span>
-      <button
-        onClick={handleAdd}>+</button>
-    </>
+    <Container>
+      <Grid
+        container
+        justifyContent={"center"}
+        alignItems={"center"}
+        direction={"column"}
+      >
+        <Typography
+          variant="h3"
+          marginTop={10}
+          color={"#FE019A"}
+        >
+          Counter App
+        </Typography>
+        <Grid
+          container
+          justifyContent={"center"}
+          alignItems={"center"}
+          marginTop={6}
+        >
+          <Grid>
+            <Button
+              sx={{
+                marginRight: 3,
+                fontSize: 30,
+                padding: 0,
+                border: "2px #FE019A solid",
+                color: "#FE019A",
+                "&:hover": {
+                  border: "2px #FE019A solid",
+                },
+                fontWeight: 700
+              }}
+              variant="outlined"
+              onClick={handleSubtract}
+            >
+              -
+            </Button>
+          </Grid>
+          <Grid>
+            <Typography
+              variant="body2"
+              fontSize={70}
+              fontWeight={200}
+              color={"#3C3E3C"}
+
+            >
+              {count}
+            </Typography>
+          </Grid>
+          <Grid>
+            <Button
+              sx={{
+                marginLeft: 3,
+                fontSize: 30,
+                padding: 0,
+                border: "2px #FE019A solid",
+                color: "#FE019A",
+                "&:hover": {
+                  border: "2px #FE019A solid",
+                },
+                fontWeight: 700
+              }}
+              variant="outlined"
+              onClick={handleAdd}
+            >
+              +
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
